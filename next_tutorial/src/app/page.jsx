2fb695b7d2fae27f7ba0b910c05a4b7e54d7fcea
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-08-13 12:08:03 +0530

    page naviagtion

--- next_tutorial/src/app/page.jsx
+++ next_tutorial/src/app/page.jsx
@@ -1,9 +1,12 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
    <>
    <h1>Home Page</h1>
+   <ul>
+    <li><Link href="/product">Product</Link></li>
+   </ul>
    </>
   );
 }
--- next_tutorial/src/app/product/page.jsx
+++ next_tutorial/src/app/product/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Link from 'next/link'
 function page() {
   return (
     <div>
       <h1>Product</h1>
+   
+
     </div>
   )
 }
